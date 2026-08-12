98b81774d72a4aa975177c7f2e9d0c59fd7b60da
Missael Apaza <missaelapaza@gmail.com>
2026-08-12 14:12:35 -0400

    rol module for assing permission

---
+++ src/rols/rols.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { RolsService } from './rols.service';
+import { RolsController } from './rols.controller';
+
+@Module({
+  controllers: [RolsController],
+  providers: [RolsService],
+})
+export class RolsModule {
+}
+
