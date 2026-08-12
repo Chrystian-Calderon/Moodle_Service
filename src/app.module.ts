52a9c12b7dd262c4835140db332fdfe19ae0ab12
Chrystian Calderon <chrystianralfc@gmail.com>
2026-08-12 14:15:47 -0400

    agregando inscripciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ConfigModule } from '@nestjs/config';
+import { InscripcionModule } from './modules/inscripcion/inscripcion.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), AuthModule, UserModule, PrismaModule],
+  }), AuthModule, UserModule, PrismaModule, InscripcionModule],
   controllers: [AppController],
   providers: [AppService],
 })
