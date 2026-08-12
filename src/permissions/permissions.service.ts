a23bac8507e08953e8a32d362d0cbdf5eda89e0a
Missael Apaza <missaelapaza@gmail.com>
2026-08-12 14:17:42 -0400

    created permission for roles

---
+++ src/permissions/permissions.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { CreatePermissionDto } from './dto/create-permission.dto';
+import { UpdatePermissionDto } from './dto/update-permission.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class PermissionsService {
+
+  constructor(private readonly prisma: PrismaService) { }
+
+  create(createPermissionDto: CreatePermissionDto) {
+    return 'This action adds a new permission';
+  }
+
+  findAll() {
+    return `This action returns all permissions`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} permission`;
+  }
+
+  update(id: number, updatePermissionDto: UpdatePermissionDto) {
+    return `This action updates a #${id} permission`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} permission`;
+  }
+
+  async createPermission(createPermissionDto: CreatePermissionDto) {
+    const { nombre, descripcion } = createPermissionDto;
+    const permission = await this.prisma.permiso.create({
+      data: {
+        nombre,
+        descripcion,
+      },
+    });
+
+    return permission;
+  }
+}
