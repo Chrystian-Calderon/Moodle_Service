acd2f7bd5bec8b7d42ac9b75e887d5d7ea5f63d3
Missael Apaza <missaelapaza@gmail.com>
2026-08-14 13:21:55 -0400

    add validation in createCursoDto

--- src/curso/dto/create-curso.dto.ts
+++ src/curso/dto/create-curso.dto.ts
@@ -1,12 +1,47 @@
+import {
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+} from 'class-validator';
+
 export class CreateCursoDto {
+  @IsString()
   nombre!: string;
+
+  @IsOptional()
+  @IsString()
   categoria?: string;
+
+  @IsString()
   slug!: string;
+
+  @IsOptional()
+  @IsString()
   descripcionCorta?: string;
+
+  @IsOptional()
+  @IsString()
   descripcionCompleta?: string;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
   duracionHoras?: number;
+
+  @IsOptional()
+  @IsString()
   rutaPortada?: string;
+
+  @IsOptional()
+  @IsString()
   rutaImagenSecundaria?: string;
+
+  @IsOptional()
+  @IsString()
   estado?: string;
+
+  @IsOptional()
+  @IsString()
   creadoPor?: string;
 }
