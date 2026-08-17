a9e1e5b94045e37ceff5cad1d904d1dd892e7f1a
Chrystian Calderon <chrystianralfc@gmail.com>
2026-08-17 09:08:29 -0400

    agregando all exception

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exception.filter';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalFilters(
-    new HttpExceptionFilter(),
     new AllExceptionsFilter(),
   );
   const port = process.env.PORT || 3000;
