395f4ccc49206a14386938a70b46c5cc1a3f3ccf
@ Alejandro_Zapata <142756173+Ale58150@users.noreply.github.com>
2026-08-21 09:39:18 -0400

    Líneas Comentadas

--- prisma/seed-all.ts
+++ prisma/seed-all.ts
@@ -12,7 +12,7 @@
 //   npx tsx prisma/seed-all.ts
 // (o npx ts-node prisma/seed-all.ts si usas ts-node)
 
-import "dotenv/config"; // asegura que DATABASE_URL se cargue desde .env aunque tsx no lo haga solo
+/*import "dotenv/config"; // asegura que DATABASE_URL se cargue desde .env aunque tsx no lo haga solo
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import * as bcrypt from "bcrypt";
@@ -739,4 +739,4 @@ main()
     })
     .finally(async () => {
         await prisma.$disconnect();
-    });
+    });*/
